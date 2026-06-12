732069f3f1495025cb79a453cc6964d84a384095
user <vinaygoudmaduri@gmail.com>
2026-06-12 12:40:53 +0530

    roboshop commit

--- Catalogue.sh
+++ Catalogue.sh
@@ -2,12 +2,12 @@ component=catalogue
 source common.sh
 
 print_head copy mangodb repo file
-cp Mango.repo /etc/yum.repos.d/mongo.repo &>> $log_file
+cp Mango.repo /etc/yum.repos.d/mongo.repo &>>$log_file
 
 nodejs_app_setup
 
 print_head install mangodb
-dnf install mongodb-mongosh -y &>> $log_file
+dnf install mongodb-mongosh -y &>>$log_file
 
 print_head load mangodb master data
-mongosh --host mango-dev.vdevops21.online </app/db/master-data.js &>> $log_file
+mongosh --host mango-dev.vdevops21.online </app/db/master-data.js &>>$log_file
