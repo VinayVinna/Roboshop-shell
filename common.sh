771b55b6ac02cd0fc1929bca6c645d974a9f13e3
user <vinaygoudmaduri@gmail.com>
2026-06-12 13:40:15 +0530

    roboshop commit

--- common.sh
+++ common.sh
@@ -13,7 +13,7 @@ systemd_setup(){
 artifact_download(){
   print_head add application user
   id roboshop &>> $log_file
-  if [ $? -ne 0]; then
+  if [ $? -ne 0 ]; then
     useradd roboshop &>> $log_file
   fi
   exit_status_print $?
